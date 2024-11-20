4676e7de265629597c45d97f5b9004e3db31223a
Aidan V <159743307+AidanVu8@users.noreply.github.com>
2024-11-21 10:31:07 +1100

    servo motor test code

--- Aidan.V/Aidan.V.ino
+++ Aidan.V/Aidan.V.ino
@@ -1,2 +1,15 @@
-Pin 6
-Pin 7
+#include <Servo.h>
+Servo myservo;
+unsigned static int servoPin = 7;
+unsigned static int potpin = A2;
+
+
+void setup() {
+  myservo.attach(servoPin);
+}
+
+void loop() {
+  int val = analogRead(potpin);
+  val = map(val, 0, 1023, 180, 0);
+  myservo.write(val);
+}
