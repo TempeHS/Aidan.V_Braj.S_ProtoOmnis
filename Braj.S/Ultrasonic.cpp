9e8cccd7726615b183f7cc3472bdb2d8b58edbb8
Braj.S <159206352+BrajSapkota@users.noreply.github.com>
2024-11-28 10:11:01 +1100

    added servo ultrasonic

--- Braj.S/Ultrasonic.cpp
+++ Braj.S/Ultrasonic.cpp
@@ -1,76 +1,75 @@
 /*
- * Ultrasonic.cpp
- *
- * Library for Ultrasonic Ranging Module in a minimalist way
- *
- * created 3 Apr 2014
- * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
- * modified 23 Jan 2017
- * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
- * modified 04 Mar 2017
- * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
- * modified 15 May 2017
- * by Eliot Lim    (github: @eliotlim)
- * modified 10 Jun 2018
- * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
- * modified 14 Jun 2018
- * by Otacilio Maia (github: @OtacilioN | linkedIn: in/otacilio)
- *
- * Released into the MIT License.
- */
+  Author: 
+  
+  Learning Intention:
+  The students will learn how to wire a four pin ultrasonic sensor and configure it to measure distance using a library.
+  
+  Success Criteria:
+    1.  I can correctly wire a 4 pin ultrasonic sensor
+    2.  I understand what the trigger and echo pins do
+    3.  I generally understand what a library is
+    4.  I can get a distance from a untra sensor
+    5.  I understand that a continuous servo speed and direction is set by a frequency signal sent from the microcontroller
+    6.  I can apply this knowledge to the Ultrasonic Sensor in the sensor kit
 
-#if ARDUINO >= 100
-  #include <Arduino.h>
-#else
-  #include <WProgram.h>
-#endif
+  Student Notes: 
+
+  Documentation: 
+    https://www.tutorialspoint.com/arduino/arduino_ultrasonic_sensor.htm <-- Ultrasonic sensor explained
+    https://github.com/ErickSimoes/Ultrasonic/tree/master <-- We are using this library
+
+  Schematic:
+    https://www.tinkercad.com/things/kngLnqo2HEU?sharecode=OVpOeJsUP3bOHBkzbkWCfGcuSIswqXiISYQiG6UzotA
+    https://github.com/TempeHS/TempeHS_Ardunio_Bootcamp/blob/main/11.ultrasonicSensor/Bootcamp-ultrasonicSensor.png
+*/
 
 #include "Ultrasonic.h"
+#include <Servo.h>
 
-Ultrasonic::Ultrasonic(uint8_t trigPin, uint8_t echoPin, unsigned long timeOut) {
-  trig = trigPin;
-  echo = echoPin;
-  threePins = trig == echo ? true : false;
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-  timeout = timeOut;
-}
+Ultrasonic sensor(5);
+Servo myServo;
 
-unsigned int Ultrasonic::timing() {
-  if (threePins)
-    pinMode(trig, OUTPUT);
+#define servoPin 7
+static signed int potpin = A2;
+unsigned long previousMillis = 0;
+const unsigned long gateInterval = 0;
 
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  //delay(30);
-
-  if (threePins)
-    pinMode(trig, INPUT);
+void setup() {
   
-  previousMicros = micros();
-  while(!digitalRead(echo) && (micros() - previousMicros) <= timeout); // wait for the echo pin HIGH or timeout
-  previousMicros = micros();
-  while(digitalRead(echo)  && (micros() - previousMicros) <= timeout); // wait for the echo pin LOW or timeout
+  myServo.attach(servoPin);
+  myServo.write(180);
 
-  return micros() - previousMicros; // duration
+  Serial.begin(9600);
+
 }
 
-/*
- * If the unit of measure is not passed as a parameter,
- * sby default, it will return the distance in centimeters.
- * To change the default, replace CM by INC.
- */
-unsigned int Ultrasonic::read(uint8_t und) {
-  return timing() / und / 2;  //distance by divisor
-}
+void loop() {
 
-/*
- * This method is too verbal, so, it's deprecated.
- * Use read() instead.
- */
-unsigned int Ultrasonic::distanceRead(uint8_t und) {
-  return read(und);
+  unsigned long currentMillis = millis();
+  int val = analogRead(potpin);  
+  Serial.println(sensor.distanceRead());
+  
+    if (sensor.distanceRead() <= 30) {    
+    myServo.write(0);
+    previousMillis = currentMillis;
+
+
+
+  } else {
+
+
+
+    if (currentMillis - previousMillis >= gateInterval) {
+
+      myServo.write(180);
+
+    }
+
+  }
+
+  //val = map(val, 0, 1023, 0, 180);
+  //myServo.write(val);
+  
+  //Serial.println(analogRead(potpin));
+
 }
