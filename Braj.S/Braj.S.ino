4a83dd9edf1083ff6df4c25094f5faa67c9bdccd
Braj.S <159206352+BrajSapkota@users.noreply.github.com>
2024-11-28 10:24:51 +1100

    Update Braj.S.ino

--- Braj.S/Braj.S.ino
+++ Braj.S/Braj.S.ino
@@ -1,44 +1,51 @@
+
+#include "Ultrasonic.h"
 #include <Servo.h>
 
-// constants won't change
-const int TRIG_PIN  = 8;  // Arduino pin connected to Ultrasonic Sensor's TRIG pin
-const int ECHO_PIN  = 8;  // Arduino pin connected to Ultrasonic Sensor's ECHO pin
-const int SERVO_PIN = 8; // Arduino pin connected to Servo Motor's pin
-const int DISTANCE_THRESHOLD = 50; // centimeters
+Ultrasonic sensor(5);
+Servo myServo;
 
-Servo servo; // create servo object to control a servo
-
-// variables will change:
-float duration_us, distance_cm;
+#define servoPin 7
+static signed int potpin = A2;
+unsigned long previousMillis = 0;
+const unsigned long gateInterval = 0;
 
 void setup() {
-  Serial.begin (9600);       // initialize serial port
-  pinMode(TRIG_PIN, OUTPUT); // set arduino pin to output mode
-  pinMode(ECHO_PIN, INPUT);  // set arduino pin to input mode
-  servo.attach(SERVO_PIN);   // attaches the servo on pin 9 to the servo object
-  servo.write(0);
+  
+  myServo.attach(servoPin);
+  myServo.write(180);
+
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // generate 10-microsecond pulse to TRIG pin
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
 
-  // measure duration of pulse from ECHO pin
-  duration_us = pulseIn(ECHO_PIN, HIGH);
-  // calculate the distance
-  distance_cm = 0.017 * duration_us;
+  unsigned long currentMillis = millis();
+  int val = analogRead(potpin);  
+  Serial.println(sensor.distanceRead());
+  
+    if (sensor.distanceRead() <= 30) {    
+    myServo.write(0);
+    previousMillis = currentMillis;
 
-  if(distance_cm < DISTANCE_THRESHOLD)
-    servo.write(90); // rotate servo motor to 90 degree
-  else
-    servo.write(0);  // rotate servo motor to 0 degree
 
-  // print the value to Serial Monitor
-  Serial.print("distance: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
 
-  delay(500);
+  } else {
+
+
+
+    if (currentMillis - previousMillis >= gateInterval) {
+
+      myServo.write(180);
+
+    }
+
+  }
+
+  //val = map(val, 0, 1023, 0, 180);
+  //myServo.write(val);
+  
+  //Serial.println(analogRead(potpin));
+
 }
